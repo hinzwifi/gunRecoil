62ff60ce6cb91177614ecf51e52e757a9cd8987c
hinzwifi <markjoshuahaynes@protonmail.com>
2022-02-07 12:34:07 +0800

    Added a process env for heroku

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const app = require("express")();
 const Gun = require("gun");
+require("dotenv").config();
 const port = 3070;
 app.use(Gun.serve);
-const server = app.listen(port);
+const server = app.listen(process.env.PORT || port);
 Gun({ web: server });
