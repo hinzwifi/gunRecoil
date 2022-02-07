eb5c130cdc5f42d3f0962bb55eb8074c000199e5
hinzwifi <markjoshuahaynes@protonmail.com>
2022-02-07 13:47:19 +0800

    Added cors but idk why

--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
 const app = require("express")();
 const Gun = require("gun");
 require("dotenv").config();
+const cors = require("cors");
+const corsOptions = {
+  origin: "*",
+};
 const port = 3070;
 app.use(Gun.serve);
+app.use(cors(corsOptions));
+
 const server = app.listen(process.env.PORT || port);
 Gun({ web: server });
