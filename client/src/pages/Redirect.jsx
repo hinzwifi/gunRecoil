f734b161af2f40bec9da6754248249046c1b8677
hinzwifi <markjoshuahaynes@protonmail.com>
2022-02-07 11:56:09 +0800

    Cleaned out the code

--- client/src/pages/Redirect.jsx
+++ client/src/pages/Redirect.jsx
@@ -26,40 +26,17 @@ function Redirect() {
   }
   useEffect(() => {
     const db = Gun("http://localhost:3070/gun");
-    // window.gun = db;
-    // var note = { url: "https://hinzwifi.xyz" };
-    // db.get("redirect").get(id).put(note);
-    // db.get("redirect")
-    //   .get(id)
-    //   .once(function (data, key) {
-    //     setview(data);
-    //   });
-    // db.get("redirect").get(id);
-
     db.get("redirect")
       .get(id)
       .once(function (data, key) {
         if (data === undefined) {
           setnothing(false);
         } else {
-          setview(data.urlViews + 1);
           SEA.decrypt(data.url, "supersecret").then((e) => {
             setredirect(e);
           });
         }
       });
-
-    // axios
-    //   .get(
-    //     "https://raw.githubusercontent.com/hinzwifi/gunRecoil/47dcdd7f9400612dc6ad77290f46be893b7661b3/client/url.json"
-    //   )
-    //   .then((res) => {
-    //     const get = res.data;
-    //     setredirect(get.url);
-    //   })
-    //   .catch((err) => {
-    //     console.log(err);
-    //   });
   }, []);
 
   return (
